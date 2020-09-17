4b53d8a5d7a0629f50aec5826a2b924d51afc7f8
KentaHatoba <kenta.hatoba@seezoo.co.jp>
2020-09-17 16:06:26 +0900

    Quiz: Make Widgets Stateful の回答を追加

--- hello_rectangle/lib/category_route.dart
+++ hello_rectangle/lib/category_route.dart
@@ -17,11 +17,12 @@ final _backgroundColor = Colors.green[100];
 /// While it is named CategoryRoute, a more apt name would be CategoryScreen,
 /// because it is responsible for the UI at the route's destination.
 // TODO: Make CategoryRoute a StatefulWidget
-class CategoryRoute extends StatelessWidget {
-  const CategoryRoute();
+class CategoryRoute extends StatefulWidget {
+  @override
+  createState() => CategoryRouteState();
+}
 
-  // TODO: Create State object for the CategoryRoute
-
+class CategoryRouteState extends State<CategoryRoute> {
   static const _categoryNames = <String>[
     'Length',
     'Area',
@@ -44,9 +45,23 @@ class CategoryRoute extends StatelessWidget {
     Colors.red,
   ];
 
-  /// Makes the correct number of rows for the list view.
-  ///
-  /// For portrait, we use a [ListView].
+  final categories = <Category>[];
+
+  void _load() {
+    for (var i = 0; i < _categoryNames.length; i++) {
+      setState(() {
+        categories.add(
+          Category(
+            name: _categoryNames[i],
+            color: _baseColors[i],
+            iconLocation: Icons.cake,
+            units: _retrieveUnitList(_categoryNames[i]),
+          ),
+        );
+      });
+    }
+  }
+
   Widget _buildCategoryWidgets(List<Widget> categories) {
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) => categories[index],
@@ -54,7 +69,6 @@ class CategoryRoute extends StatelessWidget {
     );
   }
 
-  /// Returns a list of mock [Unit]s.
   List<Unit> _retrieveUnitList(String categoryName) {
     return List.generate(10, (int i) {
       i += 1;
@@ -66,23 +80,13 @@ class CategoryRoute extends StatelessWidget {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _load();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // TODO: Instead of re-creating a list of Categories in every build(),
-    // save this as a variable inside the State object and create
-    // the list at initialization (in initState()).
-    // This way, you also don't have to pass in the list of categories to
-    // _buildCategoryWidgets()
-    final categories = <Category>[];
-
-    for (var i = 0; i < _categoryNames.length; i++) {
-      categories.add(Category(
-        name: _categoryNames[i],
-        color: _baseColors[i],
-        iconLocation: Icons.cake,
-        units: _retrieveUnitList(_categoryNames[i]),
-      ));
-    }
-
     final listView = Container(
       color: _backgroundColor,
       padding: EdgeInsets.symmetric(horizontal: 8.0),
