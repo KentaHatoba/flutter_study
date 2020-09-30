88491aa38c6a8a90eb2a6d68bee3ebf494d0fb14
KentaHatoba <kenta.hatoba@seezoo.co.jp>
2020-09-30 15:52:12 +0900

    fix: 画像の下にテキストを追加

--- udemy/mi_card/lib/main.dart
+++ udemy/mi_card/lib/main.dart
@@ -17,6 +17,14 @@ class MyApp extends StatelessWidget {
                 radius: 50.0,
                 backgroundImage: AssetImage('images/hatoba.jpg'),
               ),
+              Text(
+                'Kenta Hatoba',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ],
           ),
         ),
