e6d23ea8817a6fd6b5d57d7dd41a041db43f526d
KentaHatoba <kenta.hatoba@seezoo.co.jp>
2020-10-05 18:22:04 +0900

    fix: 画像を表示する部分を変数を利用し指定するよう変更

--- udemy/magic8ball/lib/main.dart
+++ udemy/magic8ball/lib/main.dart
@@ -39,7 +39,7 @@ class _BallState extends State<Ball> {
           ballNumber = Random().nextInt(5) + 1;
           print(ballNumber);
         },
-        child: Image.asset('images/ball1.png'),
+        child: Image.asset('images/ball$ballNumber.png'),
       ),
     );
   }
