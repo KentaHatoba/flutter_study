a124ec1f36c54c4aac5a8322f155c65fad828e83
KentaHatoba <kenta.hatoba@seezoo.co.jp>
2020-10-05 17:36:06 +0900

    fix: StatelessWidgetを作成

--- udemy/magic8ball/lib/main.dart
+++ udemy/magic8ball/lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
-      ),
-    );
+void main() {
+  runApp(
+    MaterialApp(
+      home: BallPage(),
+    ),
+  );
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
