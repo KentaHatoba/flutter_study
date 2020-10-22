ab41c2a77ce285c451f7d3945be16a86fcdda35f
KentaHatoba <kenta.hatoba@seezoo.co.jp>
2020-10-22 10:38:24 +0900

    fix: 変数questionListを作成しQuestionのオブジェクトを配列にした

--- udemy/quizzler/lib/main.dart
+++ udemy/quizzler/lib/main.dart
@@ -28,22 +28,20 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
+  List<Question> questionList = [
+    Question(
+      q: 'You can lead a cow down stairs but not up stairs.',
+      a: false,
+    ),
+    Question(
+      q: 'Approximately one quarter of human bones are in the feet.',
+      a: true,
+    ),
+    Question(
+      q: 'A slug\'s blood is green.',
+      a: true,
+    ),
   ];
-
-  List<bool> answers = [
-    false,
-    true,
-    true,
-  ];
-
-  Question q1 = Question(
-    q: 'You can lead a cow down stairs but not up stairs.',
-    a: false,
-  );
 
   int questionNumber = 0;
 
@@ -59,7 +57,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questionList[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -85,7 +83,8 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked false.
 
-                bool correctAnswer = answers[questionNumber];
+                bool correctAnswer =
+                    questionList[questionNumber].questionAnswer;
 
                 if (correctAnswer == true) {
                   print('正解!');
@@ -115,7 +114,8 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked false.
 
-                bool correctAnswer = answers[questionNumber];
+                bool correctAnswer =
+                    questionList[questionNumber].questionAnswer;
 
                 if (correctAnswer == false) {
                   print('正解!');
