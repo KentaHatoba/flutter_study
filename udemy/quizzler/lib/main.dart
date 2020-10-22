e62a8e7cc4e482d547faa73284e149b69dac6edf
KentaHatoba <kenta.hatoba@seezoo.co.jp>
2020-10-22 09:46:44 +0900

    fix: モデルクラスQuestionを利用した変数q1を作成

--- udemy/quizzler/lib/main.dart
+++ udemy/quizzler/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -38,6 +39,11 @@ class _QuizPageState extends State<QuizPage> {
     true,
     true,
   ];
+
+  Question q1 = Question(
+    q: 'You can lead a cow down stairs but not up stairs.',
+    a: false,
+  );
 
   int questionNumber = 0;
 
