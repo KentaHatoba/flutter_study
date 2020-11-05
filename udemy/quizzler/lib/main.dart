22cfeaf0ba14c40566455cef865c20bda590a170
KentaHatoba <kenta.hatoba@seezoo.co.jp>
2020-11-05 11:10:44 +0900

    fix: クイズの答えをチェックするメソッドを作成し答えをチェックする部分でそのメソッドを呼び出すように変更

--- udemy/quizzler/lib/main.dart
+++ udemy/quizzler/lib/main.dart
@@ -30,6 +30,20 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
+  void checkAnswer(bool userPickedAnswer) {
+    bool correctAnswer = quizBrain.getCorrectAnswer();
+
+    if (userPickedAnswer == correctAnswer) {
+      print('正解!');
+    } else {
+      print('残念');
+    }
+
+    setState(() {
+      quizBrain.nextQuestion();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -66,19 +80,8 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                checkAnswer(true);
                 //The user picked false.
-
-                bool correctAnswer = quizBrain.getCorrectAnswer();
-
-                if (correctAnswer == true) {
-                  print('正解!');
-                } else {
-                  print('残念');
-                }
-
-                setState(() {
-                  quizBrain.nextQuestion();
-                });
               },
             ),
           ),
@@ -97,18 +100,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
-
-                bool correctAnswer = quizBrain.getCorrectAnswer();
-
-                if (correctAnswer == false) {
-                  print('正解!');
-                } else {
-                  print('残念');
-                }
-
-                setState(() {
-                  quizBrain.nextQuestion();
-                });
+                checkAnswer(false);
               },
             ),
           ),
