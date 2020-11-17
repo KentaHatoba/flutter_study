7bf1f3a584e1c163a467e065b5670fad4e8b7af6
KentaHatoba <kenta.hatoba@seezoo.co.jp>
2020-11-17 11:23:01 +0900

    fix: ThemeDataを追加

--- udemy/bmi_calculator/lib/main.dart
+++ udemy/bmi_calculator/lib/main.dart
@@ -6,6 +6,14 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color(0xFF0A0D21),
+        scaffoldBackgroundColor: Color(0xFF0A0D21),
+        accentColor: Colors.purple,
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.white),
+        ),
+      ),
       home: InputPage(),
     );
   }
